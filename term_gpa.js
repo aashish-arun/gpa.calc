1a3a35d66ef75c21ba106a86d072e669be15c70e
aashish-arun <aashish.ouo@gmail.com>
2024-12-25 16:47:07 -0700

    Day-2

--- term_gpa.js
+++ term_gpa.js
@@ -3,11 +3,8 @@ let grade_2 = document.getElementById(grade-2)
 let grade_3 = document.getElementById(grade-3)
 let grade_4 = document.getElementById(grade-4)
 let grade_5 = document.getElementById(grade-5)
-let total_grade = document.getElementById(total-grade)
 
-function termGpa(grade_1,grade_2,grade_3,grade_4,grade_5,total_grade) {
+function termGpa(grade_1,grade_2,grade_3,grade_4,grade_5) {
     var calc_grade = (grade_1 + grade_2 + grade_3 + grade_4 + grade_5) / 5
-    total_grade = `Term GPA ${calcGrade}`
+    document.getElementById("total-grade").innerHTML = `Term GPA ${calc_grade}`;
 }
-
-
